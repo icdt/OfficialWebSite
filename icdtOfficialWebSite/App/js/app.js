ccc9e3353b6f8a5776917d6e3695b9960d028d9f
istale <iscarryon@gmail.com>
2015-04-29 20:15:09 +0800

    kevin: add base architecture

---
+++ icdtOfficialWebSite/App/js/app.js
@@ -0,0 +1,80 @@
+﻿
+var app = angular.module('demo', ['ui.router', 'restangular', 'ngToast']);
+
+app.run(['$rootScope', '$state', '$stateParams',
+        function ($rootScope, $state, $stateParams) {
+
+            $rootScope.$state = $state;
+            $rootScope.$stateParams = $stateParams;
+
+        }
+]);
+
+
+app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider',
+function ($stateProvider, $urlRouterProvider, $httpProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+          url: '/',
+          templateUrl: 'home.html',
+          controller: 'HomeCtrl'
+      })
+    ;
+
+}]);
+
+app.controller('HomeCtrl', ['$scope', 'Content', function($scope, Content){
+
+
+    $scope.vm = Content;
+
+
+}]);
+
+
+app.factory('Content', function () {
+
+    return {
+
+        logo: 'img/logo.png',
+        service: [{
+            img: '',
+            title:'App開發',
+            content: ''
+        }, {
+            img: '',
+            title: '系統整合',
+            content: ''
+        }, {
+            img: '',
+            title: '',
+            content: ''
+        }, {
+            img: '',
+            title: '',
+            content: ''
+        }]
+
+
+
+
+
+
+
+
+
+
+    };
+
+
+
+
+
+
+
+
+
+});
