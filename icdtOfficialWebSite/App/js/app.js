db880266c2a128e144e9d555d9831e1378d0bbe8
ken <arken@infocloud.com.tw>
2015-05-05 12:43:17 +0800

    ken:add project imglogo

--- icdtOfficialWebSite/App/js/app.js
+++ icdtOfficialWebSite/App/js/app.js
@@ -43,7 +43,9 @@ app.factory('Content', function () {
             
         },
 
-        logo:'img/icdtlogo.png',
+        logo: 'img/icdtlogo.png',
+        customerwordlogo: 'img/customerwordlogo.png',
+        contactuslogo: 'img/contactuslogo.png',
         service: [{
             img: 'img/websitelogo.png',
             title:'網站開發',
@@ -60,7 +62,43 @@ app.factory('Content', function () {
             img: 'img/planlogo.png',
             title: '行銷企劃',
             content: ''
+        }],
+        project: [{
+            img: 'img/p1.png',
+            title: 'LVGB2CWebSite',
+            content: ''
+        }, {
+            img: 'img/p2.png',
+            title: 'LVGB2BWebSite',
+            content: ''
+        }, {
+            img: 'img/p3.png',
+            title: 'LVGPOS',
+            content: ''
+        }, {
+            img: 'img/p4.png',
+            title: '右昌醫院',
+            content: ''
+        },
+        {
+            img: 'img/p5.png',
+            title: '佳音牙醫',
+            content: ''
+        }, {
+            img: 'img/p6.png',
+            title: '濱海SEO',
+            content: ''
+        }, {
+            img: 'img/p7.png',
+            title: '腦波遊戲APP',
+            content: ''
+        }, {
+            img: 'img/p8.png',
+            title: '熊熊超外送',
+            content: ''
+        
         }]
+       
 
     };
 
